0e0aa53a5635d9c109bc2573dbac6846e14ea929
Liu <liuqianhn@hotmail.com>
2021-05-02 12:29:34 -0400

    Improved python-to-cxx

--- Python_to_CXX_class.h
+++ Python_to_CXX_class.h
@@ -3,19 +3,28 @@
 // Python_to_CXX_class
 
 #include <string>
-#include <vector>
+//#include <vector>
 
+#define MAX_INPUT_FILES 2048;
 
 class Input_Para{
 public:
    int threads;
    
    std::string output_folder;
-   std::vector<std::string> input_files;
+   //std::vector<std::string> input_files;
+   std::string *input_files;
+   int num_input_files;
 
    std::string out_prefix;
 
    int64_t other_flags;  
+
+   /// functions list;
+   std::string add_input_file(std::string _ip_file);
+
+   Input_Para();
+   ~Input_Para();
 };
 
 #endif
