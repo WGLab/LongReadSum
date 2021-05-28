119b31271c85ecdfacc464635f185ec86fa7635a
Ahsan <umairahsan57@gmail.com>
2021-05-28 14:42:20 -0400

    utils updated

--- utils.py
+++ utils.py
@@ -23,63 +23,44 @@ def wrap(s):
         split.append(tuple(l[-(len(l)%3):]))
     return '\n'.join([' '.join(x) for x in split])
 
-def plot_read_length_stats(data, path):
-    fig, axes = plt.subplots(2, sharex=True, figsize =(8, 6))
+def plot_read_length_stats(data, path, subtitles=None, categories=None):
+    fig, axes = plt.subplots(len(data), sharex=True, figsize =(8, 6))
     
-    numbers_list=[[data.mapped_long_read_info.n50_read_length,
-                 data.mapped_long_read_info.mean_read_length,
-                 data.mapped_long_read_info.median_read_length],
-    
-                 [data.unmapped_long_read_info.n50_read_length,
-                 data.unmapped_long_read_info.mean_read_length,
-                 data.unmapped_long_read_info.median_read_length]]
-    
-    category=['N50', 'Mean', 'Median']
-    category=[wrap(x) for x in category]
-    
+    numbers_list=[[x.n50_read_length, x.mean_read_length, x.median_read_length] for x in data]
+
+    category=['N50', 'Mean', 'Median']    
     category_list=itertools.cycle([category])
     ylabel_list=itertools.cycle(['Length (bp)'])
     xlabel_list=itertools.cycle([None])
-    subtitle_list=["Mapped Reads","Unmapped Reads"]
+    subtitle_list=subtitles
     bar_plot(fig, numbers_list, category_list, xlabel_list, ylabel_list, subtitle_list, path)
         
     
-def plot_base_counts(data, path):
-    fig, axes = plt.subplots(2, sharey=True, figsize =(8, 6))
+def plot_base_counts(data, path, subtitles=None, categories=None):
+    fig, axes = plt.subplots(len(data), sharey=True, figsize =(8, 6))
 
-    numbers_list=[[data.mapped_long_read_info.total_a_cnt, 
-             data.mapped_long_read_info.total_c_cnt,
-             data.mapped_long_read_info.total_g_cnt,
-             data.mapped_long_read_info.total_tu_cnt, 
-             data.mapped_long_read_info.total_n_cnt],
-                  
-             [data.unmapped_long_read_info.total_a_cnt, 
-             data.unmapped_long_read_info.total_c_cnt,
-             data.unmapped_long_read_info.total_g_cnt,
-             data.unmapped_long_read_info.total_tu_cnt, 
-             data.unmapped_long_read_info.total_n_cnt]]
-
+    numbers_list=[[x.total_a_cnt, x.total_c_cnt, x.total_g_cnt, x.total_tu_cnt, x.total_n_cnt] for x in data]
+    
     category_list=itertools.cycle([['A', 'C', 'G', 'T/U', 'N']])
     ylabel_list=itertools.cycle(['Base'])
     xlabel_list=itertools.cycle(['Counts'])
-    subtitle_list=["Mapped Reads","Unmapped Reads"]
+    subtitle_list=subtitles
     bar_plot(fig, numbers_list, category_list, xlabel_list, ylabel_list, subtitle_list, path)
     #lrst_global.plot_filenames['base_st']['summary']='GC Content: {:.2%}'.format(bam_output.mapped_long_read_info.gc_cnt)
 
-def plot_basic_info(data_1,data_2, categories, path):
+def plot_basic_info(data, path, subtitles=None, categories=None):
     fig, axes= plt.subplots(2,2, figsize =(8, 6))
     
 
-    numbers_list=[( data_1.total_num_reads,  data_2.total_num_reads),
-             (data_1.total_num_bases,  data_2.total_num_bases),
-             (data_1.longest_read_length,  data_2.longest_read_length),
-             (data_1.gc_cnt,  data_2.gc_cnt)]
+    numbers_list=[[x.total_num_reads, x.total_num_bases, x.longest_read_length, x.gc_cnt] for x in data]
+    
+    numbers_list=zip(*numbers_list)
     
     category_list=itertools.cycle([categories])
     subtitle_list=['Number of Reads', 'Number of Bases', 'Longest Read', 'GC Content']
-    ylabel_list=['Count', 'Count', 'Length (bp)', '%']
-    xlabel_list=itertools.cycle([None])
-    bar_plot(fig, numbers_list, category_list, xlabel_list, ylabel_list, subtitle_list, path)
+    xlabel_list=['Count', 'Count', 'Length (bp)', '%']
+    ylabel_list=itertools.cycle([None])
+    bar_plot(fig, numbers_list, category_list, xlabel_list, ylabel_list, subtitle_list, path, orientation='h')
     
     
 def bar_plot(fig, numbers_list,category_list, xlabel_list, ylabel_list, subtitle_list, path, orientation='v', print_value =True):
