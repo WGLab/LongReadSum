6f060620e61aa11c7457ed2eab662cf4700fcd77
Liu <liuqianhn@hotmail.com>
2021-05-28 15:46:23 -0400

    add more para & fixed errors

--- plot_for_F5.py
+++ plot_for_F5.py
@@ -11,8 +11,6 @@ def f5_plot( f5_output, para_dict ):
     out_path=para_dict["output_folder"]
     get_image_path=lambda x: os.path.join(out_path,lrst_global.plot_filenames[x]['file'])
   
-    print("test array {}".format( f5_output.f5_long_read_info.read_quality_distribution[4]))
- 
     plot_read_length_stats([f5_output.f5_long_read_info.long_read_info, f5_output.f5_passed_long_read_info.long_read_info, f5_output.f5_failed_long_read_info.long_read_info], get_image_path('read_length_st'), subtitles=['All Reads', 'Passed Reads', 'Failed Reads'])
     
     plot_base_counts([f5_output.f5_long_read_info.long_read_info, f5_output.f5_passed_long_read_info.long_read_info, f5_output.f5_failed_long_read_info.long_read_info], get_image_path('base_st'), subtitles=['All Reads', 'Passed Reads', 'Failed Reads'])
