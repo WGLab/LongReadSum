f1f28287606f852841f117dd07f38e50ff369c66
perdomoj <jonperdomodb@gmail.com>
2022-06-30 12:44:17 -0400

    ENH: Refactor pytest structure

--- LRST_function.cpp
+++ LRST_function.cpp
@@ -18,24 +18,12 @@ int generate_statistic_from_fq(Input_Para &_input_data, Output_FQ &py_output_fq)
    return 0;
 }
 
-int generate_statistic_from_fa(Input_Para &_input_data, Output_FA &py_output_fa)
-{
-//   std::cout << "FA Module entered." << std:: endl;
-//   qc_fasta_files(_input_data, py_output_fa);
-   return 0;
-}
-
 int callFASTAModule(Input_Para& _input_data, Output_FA &py_output_fa)
 {
     int exit_code;
     exit_code = qc_fasta_files(_input_data, py_output_fa);
     return exit_code;
 }
-
-//int callFASTAModule(Input_Para& _input_data)
-//{
-//    return 0;
-//}
 
 int generate_statistic_from_f5(Input_Para &_input_data, Output_F5 &py_output_f5)
 {
--- test_statistics.py
+++ test_statistics.py
@@ -6,25 +6,31 @@ import os
 import lrst
 import pytest
 
-
-def test_fasta_n50():
+@pytest.fixture(scope='class')
+def fasta_output():
     """
-    Test that the calculated N50 value is correct for FASTA files.
+    Run the FASTA module.
     """
-
     # Set parameters
-    input_para = lrst.Input_Para()
+    default_parameters = lrst.Input_Para()
     output_folder = os.path.abspath(str("output/"))
-    input_para.output_folder = output_folder
+    default_parameters.output_folder = output_folder
     input_file = os.path.abspath(str("SampleData/fasta_trim1.fa"))  # Remote path
-    # input_file = str("/home/perdomoj/github/LongReadSum/SampleData trimmed/SampleData/fasta_trim1.fa") # Local path
-    input_para.add_input_file(input_file)
-    input_para.out_prefix = str("fa_")
+    # input_file = str("/home/perdomoj/github/LongReadSum/SampleData trimmed/SampleData/fasta_trim1.fa") # Local path    default_parameters.add_input_file(input_file)
+    default_parameters.add_input_file(input_file)
+    default_parameters.out_prefix = str("fa_")
 
     # Run the FASTA statistics module
-    fa_output = lrst.Output_FA()
-    exit_code = lrst.callFASTAModule(input_para, fa_output)
+    output = lrst.Output_FA()
+    exit_code = lrst.callFASTAModule(default_parameters, output)
 
-    # Ensure the N50 value is correct
-    n50_read_length = fa_output.long_read_info.n50_read_length
-    assert (exit_code == 0) and (n50_read_length == 9726200)
+    yield [exit_code, output]
+
+class TestFASTA:
+    def test_module_load(self, fasta_output):
+        exit_code, output_statistics = fasta_output
+
+        # Ensure the N50 value is correct
+        n50_read_length = output_statistics.long_read_info.n50_read_length
+        assert (exit_code == 0) and (n50_read_length == 9726200)
+
