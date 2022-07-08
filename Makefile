87853d226b542d630b7bfbbcf209369a4451406a
perdomoj <jonperdomodb@gmail.com>
2022-07-08 16:08:56 -0400

    STYLE: Move C++ files to /src/module_name

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-SRC_DIR := $(CURDIR)/src
+SRC_DIR := $(CURDIR)/src/longreadsum
 #SRC_DIR := src
 # $(info $(SRC_DIR))
 
 all:
     # Generate the SWIG python <-> C++ wrappers (Both ways)
     # The Python wrapper is stored in the main directory, while the C++ wrapper code is stored in /src
-	swig -c++ -python -outdir . -I$(SRC_DIR) -o $(SRC_DIR)/lrst_wrap.cxx lrst.i
+	swig -c++ -python -outdir $(SRC_DIR) -I$(SRC_DIR) -o $(SRC_DIR)/lrst_wrap.cxx lrst.i
 
 	# Compile the C++ shared libraries
 	# The C++ wrapper binary _lrst is generated in the main directory
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
+"""
+setup.py:
+Compile the module and its dependencies.
+"""
+
+import os
 from distutils.core import setup, Extension
+
 print("Running setup.py...")
+
+# Get the C++ dependencies
+project_dir = 'src/longreadsum'
+project_src_files = []
+for file in os.listdir(project_dir):
+    if file.endswith(".cxx") or file.endswith(".cpp"):
+        src_filepath = os.path.join(project_dir, file)
+        project_src_files.append(src_filepath)
+
 lrst_mod = Extension("_lrst",
-                     sources=["src/lrst_wrap.cxx", "src/Python_to_CXX_class.cpp", "src/CXX_to_python_class.cpp", "src/LRST_function.cpp","src/BAM_module.cpp", "src/BamReader.cpp", "src/F5_module.cpp", "src/kseq.cpp", "src/FASTQ_module.cpp", "src/ComFunction.cpp", "src/FASTA_module.cpp", "src/ComStruct.cpp" ],
+                     sources=project_src_files,
                      language='c++',
                      extra_compile_args=['-std=c++14'],
                      libraries=["rt", "pthread", "z", "dl", "m", "hts"],
-                     include_dirs=['src'])
+                     include_dirs=[project_dir])
 
 setup(name = "lrst",
       version = '0.1',
       author      = "Qian Liu",
       description = """Long-read statistics""",
       ext_modules=[lrst_mod],
-      py_modules=['lrst'])
+      py_modules=['lrst'],
+      test_suite='tests')
