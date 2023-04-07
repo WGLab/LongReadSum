43ab05a9cdd66abaa8c1cb200d7f1e682c802a94
perdomoj <jonperdomodb@gmail.com>
2023-04-07 16:37:01 -0400

    COMP: Test SWIG library import

--- conda/build.sh
+++ conda/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS=${LDFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
-export CPATH=${CPATH}" -I${PREFIX}/include -L${PREFIX}/lib"
-export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
-export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
-export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
+#export CFLAGS="$CFLAGS -I$PREFIX/include"
+#export LDFLAGS=${LDFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
+#export CPATH=${CPATH}" -I${PREFIX}/include -L${PREFIX}/lib"
+#export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
+#export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
+#export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
+#export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
 
 # Generate the SWIG files
 swig -c++ -python -outdir "${SRC_DIR}"/lib -I"${SRC_DIR}"/include -o "${SRC_DIR}"/src/lrst_wrap.cpp "${SRC_DIR}"/src/lrst.i
@@ -26,11 +26,12 @@ cp -r "${SRC_DIR}"/src/*.py "${PREFIX}"/lib/longreadsum
 # Copy the SWIG generated library to the lib directory
 #cp -r "${SRC_DIR}"/lib/*.so "${PREFIX}"/lib/longreadsum
 cp -r "${SRC_DIR}"/lib/*.py "${PREFIX}"/lib/longreadsum
-cp -r "${SRC_DIR}"/lib/*.so "${PREFIX}"/lib
+#cp -r "${SRC_DIR}"/lib/*.so "${PREFIX}"/lib
+cp -r "${SRC_DIR}"/lib/*.so "${PREFIX}"/lib/longreadsum
 #cp -r "${SRC_DIR}"/lib/*.py "${PREFIX}"/lib
 
 echo "Lib contents: "
 ls -l "${PREFIX}"/lib
 
-echo "Longreadsum ib contents: "
+echo "Longreadsum lib contents: "
 ls -l "${PREFIX}"/lib/longreadsum
